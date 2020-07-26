ba879332219a28f4d4e23d9c8aa4bba30ca730dc
TOBOTOMAS31 <thomas.couderc5@gmail.com>
2020-07-26 21:08:47 +0200

    modification route login

--- back/src/routes/loginRouter.js
+++ back/src/routes/loginRouter.js
@@ -8,10 +8,10 @@ const queryAsync = util.promisify(connection.query).bind(connection);
 const router = express.Router({ mergeParams: true });
 
 router.post('', async (req, res) => {
-  const { admin_email, admin_password } = req.body;
+  const { admin_mail, admin_password } = req.body;
   console.log('ICI', req.body);
   try {
-    const result = await queryAsync('SELECT * FROM admin WHERE admin_mail = ?', admin_email);
+    const result = await queryAsync('SELECT * FROM admin WHERE admin_mail = ?', admin_mail);
     const test = bcrypt.compareSync(admin_password, result[0].admin_password); // true
     if (test) {
       res.status(200).json({ message: 'OK' });
